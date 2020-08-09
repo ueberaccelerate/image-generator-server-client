20165ac4090b81bd3b49f031c2ae8ca68f0b6444
ueberaccelerate <ueberaccelerate@gmail.com>
2020-08-09 10:44:10 +0300

    -- updated color palette -- added interact with server

--- src/qt-viewreciever/gui/recivermainwindow.cpp
+++ src/qt-viewreciever/gui/recivermainwindow.cpp
@@ -1,6 +1,7 @@
 #include "recivermainwindow.h"
 #include "./ui_recivermainwindow.h"
 
+#include <QStyleFactory>
 #include <QtDebug>
 #include <QImage>
 #include <QGraphicsPixmapItem>
@@ -22,9 +23,13 @@ ReciverMainWindow::ReciverMainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::ReciverMainWindow)
     , isConnected(false)
+    , io_context()
+    , socket(io_context)
 {
     ui->setupUi(this);
     setFixedSize(width(),height());
+    setRecieverStyle();
+    setDefaultValues();
 
     std::vector<unsigned char> buffer = generate_image(Size{512,512});
 
@@ -35,6 +40,14 @@ ReciverMainWindow::ReciverMainWindow(QWidget *parent)
     scene->setSceneRect(image.rect());
     ui->graphicsView->setScene(scene);
     image.save("frame1.png");
+
+    connect(this, &ReciverMainWindow::errorAddress,
+                         this, &ReciverMainWindow::handleErrorAddress);
+    connect(this, &ReciverMainWindow::errorConnection,
+                         this, &ReciverMainWindow::handleErrorConnection);
+    connect(this, &ReciverMainWindow::successConnection,
+                         this, &ReciverMainWindow::handleSuccessConnection);
+
 }
 
 ReciverMainWindow::~ReciverMainWindow()
@@ -44,7 +57,104 @@ ReciverMainWindow::~ReciverMainWindow()
 
 void ReciverMainWindow::on_connectionButton_clicked()
 {
-    isConnected = !isConnected;
+    boost::system::error_code error;
+    if (isConnected) {
+        socket.close(error);
+        if (error) {
+            emit errorCloseSocket();
+        }
+        updateConnectionStatus(false);
+        return;
+    }
+    try
+    {
+        const boost::asio::ip::address_v4 ep = boost::asio::ip::make_address_v4(ui->ipLineEdit->text().toStdString(),error);
+        const auto port = ui->portSpinBox->value();
+        if (error) {
+            emit errorAddress();
+            return ;
+        }
+        socket.connect(tcp::endpoint(boost::asio::ip::address(ep), port), error);
+
+        if (error) {
+            emit errorConnection();
+            return ;
+        }
+        emit successConnection();
+    }
+    catch (std::exception& e)
+    {
+      qDebug() << e.what();
+    }
+}
+
+void ReciverMainWindow::handleErrorAddress()
+{
+    setInfo("handleErrorAddress");
+    updateConnectionStatus(false);
+}
+
+void ReciverMainWindow::handleErrorConnection()
+{
+    setInfo("handleErrorConnection");
+    updateConnectionStatus(false);
+
+}
+
+void ReciverMainWindow::handleCloseSocket()
+{
+    setInfo("handleCloseSocket");
+    updateConnectionStatus(false);
+}
+
+void ReciverMainWindow::handleSuccessConnection()
+{
+    setInfo("handleSuccessConnection");
+    updateConnectionStatus(true);
+}
+
+void ReciverMainWindow::setRecieverStyle()
+{
+    qApp->setStyle(QStyleFactory::create("Fusion"));
+
+    QPalette darkPalette;
+    darkPalette.setColor(QPalette::Window, QColor(53,53,53));
+    darkPalette.setColor(QPalette::WindowText, Qt::white);
+    darkPalette.setColor(QPalette::Base, QColor(25,25,25));
+    darkPalette.setColor(QPalette::AlternateBase, QColor(53,53,53));
+    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+    darkPalette.setColor(QPalette::Text, Qt::white);
+    darkPalette.setColor(QPalette::Button, QColor(53,53,53));
+    darkPalette.setColor(QPalette::ButtonText, Qt::white);
+    darkPalette.setColor(QPalette::BrightText, Qt::red);
+    darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+
+    darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+
+    qApp->setPalette(darkPalette);
+
+    qApp->setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
+    auto customPalette = ui->graphicsView->palette();
+    customPalette.setColor(QPalette::Window, Qt::white);
+    customPalette.setColor(QPalette::Base, Qt::white);
+    ui->graphicsView->setPalette(customPalette);
+}
+
+void ReciverMainWindow::setDefaultValues()
+{
+    ui->ipLineEdit->setText("127.0.0.1");
+    ui->portSpinBox->setValue(1234);
+}
+
+void ReciverMainWindow::setInfo(const QString &info)
+{
+    ui->infoLabel->setText(info);
+}
+
+void ReciverMainWindow::updateConnectionStatus(bool connected)
+{
+    isConnected = connected;
     ui->connectionButton->setText(isConnected ? "Disconnect" : "Connect");
-    qDebug() << (isConnected ? "Disconnect" : "Connect");
 }
--- src/qt-viewreciever/gui/recivermainwindow.h
+++ src/qt-viewreciever/gui/recivermainwindow.h
@@ -2,11 +2,16 @@
 #define RECIVERMAINWINDOW_H
 
 #include <QMainWindow>
+#include <QString>
+
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class ReciverMainWindow; }
 QT_END_NAMESPACE
 
+using boost::asio::ip::tcp;
 class ReciverMainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -14,12 +19,29 @@ class ReciverMainWindow : public QMainWindow
 public:
     ReciverMainWindow(QWidget *parent = nullptr);
     ~ReciverMainWindow();
+signals:
+    void errorAddress();
+    void errorConnection();
+    void errorCloseSocket();
 
+    void successConnection();
 private slots:
     void on_connectionButton_clicked();
+    void handleErrorAddress();
+    void handleErrorConnection();
+    void handleCloseSocket();
+    void handleSuccessConnection();
 
 private:
     Ui::ReciverMainWindow *ui;
     bool isConnected;
+    boost::asio::io_context io_context;
+    tcp::socket socket;
+
+    void setRecieverStyle();
+    void setDefaultValues();
+    void setInfo(const QString &info);
+    void updateConnectionStatus(bool /*connected*/);
+
 };
 #endif // RECIVERMAINWINDOW_H
